5fe762028d5848d9467aeb578bb98a4692389fc0
Brandon Yan <brandonyannn@gmail.com>
2021-03-08 15:34:41 -0500

    added css for the shortened url table and to have shortened url to be clickable and redirects to the full url

--- urlshortenerv2/src/components/ShortenedURL.js
+++ urlshortenerv2/src/components/ShortenedURL.js
@@ -6,15 +6,39 @@ import {
   FullUrlColumn,
   ShortenedURLColumn,
   CopyColumn,
+  ShortURL,
+  CopyButton
 } from "./styles/ShortenedURLStyle";
+
+// const onURLClick = () =>{
+//   // Fetch API to direct to link
+//   const {data} = axios({
+//     method: 'post',
+//     url: 'http://localhost:8080',
+//     data: {
+//       url: fullURL
+//     }
+//   })
+//   .then(response => addURL(fullURL, response.data));
+// }
 
 const ShortenedURL = (props) => {
   return (
     <ShortenedURLContainer>
       <Row>
         <FullUrlColumn>{props.fullURL}</FullUrlColumn>
-        <ShortenedURLColumn>{props.shortURL}</ShortenedURLColumn>
-        <CopyColumn>Copy</CopyColumn>
+        <ShortenedURLColumn>
+          <ShortURL href={"http://" + props.shortURL}>
+            {props.shortURL}
+          </ShortURL>
+        </ShortenedURLColumn>
+        <CopyColumn>
+          <CopyButton onClick={() => {
+            navigator.clipboard.writeText(props.shortURL)
+          }}>
+            Copy
+          </CopyButton>
+        </CopyColumn>
       </Row>
     </ShortenedURLContainer>
   );
--- urlshortenerv2/src/components/styles/ShortenedURLStyle.js
+++ urlshortenerv2/src/components/styles/ShortenedURLStyle.js
@@ -12,13 +12,26 @@ export const Row = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
+    align-items: center;
 `
 
 export const FullUrlColumn = styled.div`
+    width: 500px;
+    white-space: nowrap;
+    overflow: hidden;
 `
 
 export const ShortenedURLColumn = styled.div`
+    text-align: left;
+    margin-left: 50px;
+`
+
+export const ShortURL = styled.a`
 `
 
 export const CopyColumn = styled.div`
+    margin-left: 50px;
+`
+
+export const CopyButton = styled.button`
 `
